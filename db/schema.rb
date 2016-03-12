2f0f1b748518476bf23d4dab7f736f25cefcaf79
Ogheneovie Ralph Otutu <phexcom@phexcom.me>
2016-03-12 14:27:53 +0000

    Create chefs and recipes association and tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160306082520) do
+ActiveRecord::Schema.define(version: 20160312134725) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20160306082520) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "chef_id"
   end
 
 end
